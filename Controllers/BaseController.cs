29fe066a05604cb3dc8d0995670ae6f500bba2cf
Christian Politis <christianpolitis@gmail.com>
2025-03-27 21:38:28 +1100

    Created Base control and did scalar redirect
    #7

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VetCheckupAPI.Models;
+
+namespace VetCheckupAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BaseController : ControllerBase
+{
+    public IMediator Mediator { get; } = null!;
+
+    protected ActionResult HandleResult<T>(Result<T>? result)
+    {
+        if (result == null)
+            return NotFound();
+        if (result.IsSuccess && result.Value != null)
+            return Ok(result.Value);        
+        if (result.IsSuccess && result.Value == null)
+            return NotFound();
+        if (!result.IsSuccess && string.IsNullOrEmpty(result.Error))
+           return BadRequest("An unknown error occurred.");
+
+        return BadRequest(result.Error);
+    }
+}
