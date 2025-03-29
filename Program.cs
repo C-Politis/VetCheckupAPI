2ae05355a103669b7d50dcce438245e02b709324
Jacob Tuckwell <73861160+Necropixel15616@users.noreply.github.com>
2025-03-29 12:29:01 +1100

    Set Up Dependency Injection for the API
    Also Add Database Connection String\
    #5

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Scalar.AspNetCore;
+using VetCheckup.Application;
+using VetCheckup.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
